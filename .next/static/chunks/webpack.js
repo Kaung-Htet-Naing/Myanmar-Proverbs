dc34a23d0ad922a0908527ea811041417c8f3947
Kaung-Htet-Naing <11kaungkaung99@gmail.com>
2020-09-21 14:13:59 +0630

    initial commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3acd01f78a26877ad5e8";
+/******/ 	var hotCurrentHash = "42a364878b324dfb6b15";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
